cc5fb499e9d3dfcbb72903b85cab81ef658d6a0d
VarshaKakumanu <VarshaKakumanu@gmail.com>
2024-07-30 13:20:50 +0530

    build updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -11,14 +10,16 @@ export default defineConfig({
     },
   },
   build: {
+    sourcemap: true,
+    chunkSizeWarningLimit: 5000,
     rollupOptions: {
-        output:{
-            manualChunks(id) {
-                if (id.includes('node_modules')) {
-                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                }
-            }
-        }
-    }
-}
-})
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+  },
+});
