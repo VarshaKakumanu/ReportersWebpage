3eb1c260f46aaf3ccfa4bb41fe53da30b8a2f42d
Shivakumar N <shivakumar.n@etvbharat.com>
2024-08-24 06:21:32 +0000

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,24 @@
-import path from 'path';
+import path from "path";
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
+  server: {
+    host: '0.0.0.0',
+    port: 3000 // Optional: Specify the port if needed
+  },
+  build: { 
+    chunkSizeWarningLimit: 5000,
     rollupOptions: {
       output: {
-        manualChunks(id) {
+        manualChunks(id)
+ {
           if (id.includes('node_modules')) {
             return id.toString().split('node_modules/')[1].split('/')[0].toString();
           }
