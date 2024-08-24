d35b27180b84de7ce4616cc435856a1b69bae655
Shivakumar N <shivakumar.n@etvbharat.com>
2024-08-24 06:38:04 +0000

    Updated base vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,19 +11,24 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
-    port: 3000 // Optional: Specify the port if needed
+    port: 3000, // Optional: Specify the port if needed
   },
   build: { 
     chunkSizeWarningLimit: 5000,
     rollupOptions: {
       output: {
-        manualChunks(id)
- {
+        manualChunks(id) {
           if (id.includes('node_modules')) {
             return id.toString().split('node_modules/')[1].split('/')[0].toString();
           }
         },
       },
     },
+    // Ensure the base is set correctly if serving from a subdirectory
+    base: '/reporters-app/', // Adjust this path if necessary
+  },
+  // Configure fallback for SPA routing
+  esbuild: {
+    // You may configure additional options for ESBuild if necessary
   },
 });
