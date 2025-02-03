3dba1032a00ecc5331c2bb0adf6e9e884b6203df
VarshaTIN70 <varsha.kakumanu@vibhotech.com>
2025-02-03 16:04:33 +0530

    Updates

---
+++ assets/mime-match-CT5f1812.js
@@ -0,0 +1,1 @@
+import{g as m}from"./@transloadit-9E5jGfYO.js";import{w as o}from"./wildcard-BBmfBeJl.js";var s=o,c=/[\/\+\.]/,l=function(e,r){function t(i){var a=s(i,e,c);return a&&a.length>=2}return r?t(r.split(";")[0]):t};const f=m(l);export{f as m};
